e463250d795ca434f341c27f56dd10ee75e011f7
MEGA <MEGA@gmail.com>
2025-03-18 17:36:55 +0200

    Add project files.

---
+++ zadachki18.03/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadachki18._03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> q = new Queue<int>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                q.Enqueue(num);
+            }
+
+            int[] reversed = q.ToArray();
+            Array.Reverse(reversed);
+            Console.WriteLine(string.Join(" ", reversed));
+        }
+    }
+}
