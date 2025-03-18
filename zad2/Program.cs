0fc2686b03503105deebe9088bad8b66a46ce317
MEGA <MEGA@gmail.com>
2025-03-18 17:49:52 +0200

    char

---
+++ zad2/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad2
+{
+    internal class Program
+    {
+        static bool isPalindrome(string word)
+        {
+            Queue<char> q = new Queue<char>();
+
+            foreach (char c in word) 
+            {
+                q.Enqueue(c);
+            }
+
+            string reversed = "";
+            while (q.Count > 0)
+            {
+                reversed = q.Dequeue() + reversed;
+            }
+            
+            return reversed == word;
+        }
+
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            if (isPalindrome(input))
+                Console.WriteLine("Думата е палиндром.");
+            else
+                Console.WriteLine("Думата не е палиндром.");
+        }
+    }
+}
