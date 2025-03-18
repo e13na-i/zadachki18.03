1c2ee4484ff5381633c63f3f41b01b3ccae11a5b
MEGA <MEGA@gmail.com>
2025-03-18 18:16:42 +0200

    strrrrrr

---
+++ 3zad/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3zad
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Stack<string> stack = new Stack<string>();
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input.StartsWith("write "))
+                {
+                    string word = input.Substring(6);
+                    stack.Push(word);
+                }
+                else if (input == "undo")
+                {
+                    stack.Pop();
+                }
+
+                Console.WriteLine(string.Join(" ", stack));
+            }
+
+        }
+    }
+}
